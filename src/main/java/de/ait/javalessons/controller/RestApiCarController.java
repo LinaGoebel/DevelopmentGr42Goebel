46da090bb14b72962d07d30460006515c0cd1a58
LinaGoebel <galina283@freenet.de>
2025-03-04 20:33:46 +0100

    add RestApiCarController

--- src/main/java/de/ait/javalessons/controller/RestApiCarController.java
+++ src/main/java/de/ait/javalessons/controller/RestApiCarController.java
@@ -1,6 +1,7 @@
 package de.ait.javalessons.controller;
 
 import de.ait.javalessons.model.Car;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,27 +17,43 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/cars")
+@Slf4j // Аннотация Lombok для автоматического создания логгера
+@RestController // Аннотация Spring, указывающая, что этот класс является REST-контроллером
+@RequestMapping("/cars") // Базовый путь для всех методов в этом контроллере
 public class RestApiCarController {
 
+    // Список для хранения автомобилей
     private List<Car> carList = new ArrayList<>();
 
+    // Конструктор класса, инициализирующий список автомобилей
     public RestApiCarController() {
-        carList.addAll(List.of(
-                new Car("1", "BMW M1"),
-                new Car("2", "Audi A8"),
-                new Car("3", "Kia Spartage"),
-                new Car("4", "Volvo 960")
-        ));
+        carList.addAll(
+                List.of(
+                        new Car("1", "BMW M1"),
+                        new Car("2", "Audi A8"),
+                        new Car("3", "Kia Spartage"),
+                        new Car("4", "Volvo 960")
+                ));
     }
 
-    // @RequestMapping(value = "/cars", method = RequestMethod.GET)
+    /**
+     * Метод для получения всех автомобилей.
+     * GET-запрос на /cars
+     *
+     * @return список всех автомобилей
+     */
     @GetMapping
     Iterable<Car> getCars() {
         return carList;
     }
 
+    /**
+     * Метод для получения автомобиля по его ID.
+     * GET-запрос на /cars/{id}
+     *
+     * @param id идентификатор автомобиля
+     * @return Optional, содержащий автомобиль, если он найден, иначе пустой Optional
+     */
     @GetMapping("/{id}")
     Optional<Car> getCarById(@PathVariable String id) {
         for (Car car : carList) {
@@ -47,12 +64,28 @@ public class RestApiCarController {
         return Optional.empty();
     }
 
+    /**
+     * Метод для добавления нового автомобиля.
+     * POST-запрос на /cars
+     *
+     * @param car объект автомобиля, переданный в теле запроса
+     * @return добавленный автомобиль
+     */
     @PostMapping
     Car postCar(@RequestBody Car car) {
         carList.add(car);
         return car;
     }
 
+    /**
+     * Метод для обновления существующего автомобиля по его ID.
+     * PUT-запрос на /cars/{id}
+     *
+     * @param id  идентификатор автомобиля, который нужно обновить
+     * @param car объект автомобиля с новыми данными
+     * @return ResponseEntity с обновлённым автомобилем и статусом OK, если автомобиль найден,
+     * или с новым автомобилем и статусом CREATED, если автомобиль не найден
+     */
     @PutMapping("/{id}")
     ResponseEntity<Car> putCar(@PathVariable String id, @RequestBody Car car) {
         int index = -1;
@@ -62,16 +95,20 @@ public class RestApiCarController {
                 carList.set(index, car);
             }
         }
-        // return (index == -1 ? postCar(car) : carList.get(index));
+
         return (index == -1) ?
                 new ResponseEntity<>(postCar(car), HttpStatus.CREATED) :
                 new ResponseEntity<>(car, HttpStatus.OK);
     }
 
-
+    /**
+     * Метод для удаления автомобиля по его ID.
+     * DELETE-запрос на /cars/{id}
+     *
+     * @param id идентификатор автомобиля, который нужно удалить
+     */
     @DeleteMapping("/{id}")
     void deleteCar(@PathVariable String id) {
         carList.removeIf(car -> car.getId().equals(id));
     }
-
 }
